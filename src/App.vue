07ba0b3f32324a64b1190e01bd9af1624d46c42b
Milen Radkov <milenradkov@me.com>
2020-12-04 19:15:06 +0200

    shenko meme

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Meme />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Meme from './components/Meme.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Meme,
+  },
+};
 </script>
 
 <style>
